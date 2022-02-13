afbd55fd218e85ba9e8cf8d07336702861301fd9
huffmanks <huffmanks2@gmail.com>
2022-02-12 21:23:14 -0500

    token mods

--- utils/sendToken.util.js
+++ utils/sendToken.util.js
@@ -6,8 +6,10 @@ export const sendToken = (res, statusCode, user, refreshToken, accessToken, mess
         .status(statusCode)
         .json({
             user: {
+                id: user._id,
                 firstName: user?.firstName,
                 email: user.email,
+                team: user.team,
                 role: user.role,
                 profileImage,
             },
